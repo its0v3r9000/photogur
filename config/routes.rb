9225740b6291c4ea3a8077c6582bbf850da3adf5
Cody <codyjjwbrown@gmail.com>
2015-04-16 15:27:21 -0400

    make a wildcard for pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
 
   # Ignore the comments below for now
   # They are just documentation
