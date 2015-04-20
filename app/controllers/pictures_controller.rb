8b983600b880eec9217d00abff01b12be8da4a15
Cody <codyjjwbrown@gmail.com>
2015-04-20 10:07:30 -0400

    step 11

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,6 +8,11 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+  def show
+  	@picture = Picture.find(params[:id])
+  end
+
+
   def create
     # make a new picture with what picture_params returns (which is a method we're calling)
     @picture = Picture.new(picture_params)
@@ -24,5 +29,18 @@ class PicturesController < ApplicationController
   def picture_params
     params.require(:picture).permit(:artist, :title, :url)
   end
+
+def edit
+    @picture = Picture.find(params[:id])
+  end
+
+def update
+@picture = Picture.find(params[:id])
+
+	if @picture.update_attributes(picture_params)
+	  redirect_to "/pictures/#{@picture.id}"
+	else
+	  render :edit
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
