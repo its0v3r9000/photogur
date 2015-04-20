de21a312485fa2f7ceb5dca6f6607ab2bcf6908f
Cody <codyjjwbrown@gmail.com>
2015-04-20 10:22:16 -0400

    all of the steps
    '

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-
+  resources :pictures
 
   # Ignore the comments below for now
   # They are just documentation
